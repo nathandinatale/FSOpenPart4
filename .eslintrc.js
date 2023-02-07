c53a6fd007bd153f787c3c2e2304371ff2a6a33c
nathandinatale <ndinatal@uwo.ca>
2023-02-07 11:12:10 +0200

    Exercise 4.1

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+// changed a couple of the rules from the course outline to synchronize with my prettier settings.
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    eqeqeq: "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
+  },
+};
